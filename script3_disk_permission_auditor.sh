a550f7ddc94c85dea920cd38fa95a8765128e54b
BHaveSH290-del <bhaveshmalik2006@gmail.com>
2026-04-02 11:55:16 +0530

    Update script3_disk_permission_auditor.sh

--- script3_disk_permission_auditor.sh
+++ script3_disk_permission_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 3: Disk and Permission Auditor
-# Author: Ayush Pandey | Reg No: 24BCE11356
+# Author: bhavesh malik | Reg No: 24BCE10162
 # Course: Open Source Software | OSS NGMC Capstone Project
 # Description: Loops through key system directories, reports
 #              their permissions, owner, group, and disk usage.
